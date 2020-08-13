1cb71dc59c97cbcdc2dc765128ebedecec4336d4
Jason Chilcott <jasonchilcott@me.com>
2020-08-13 00:29:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,4 +1,6 @@
 # Write your code below game_hash
+require "pry"
+
 def game_hash
   {
     home: {
@@ -127,3 +129,11 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored(player)
+  game_hash.each do |team|
+    team.each do |key, value|
+      :player_name == player
+      binding.pry
+    end
+  end
+end
